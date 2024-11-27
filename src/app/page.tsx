2e2ebc53521abc503086dd4b4bd58ec53558b3ce
huyixi <hi@huyixi.com>
2024-11-27 13:36:18 +0800

    feat: basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client"
+import { useRef, useState } from 'react'
+
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
+  const [text, setText] = useState('')
+  const cardRef = useRef(null)
+
   return (
     <div className="">
-      <main className="">
-        <Button>Click Me</Button>
+      <main className="p-4">
+        <h2>Preview</h2>
+        <div ref={cardRef}>
+          {text || "Preview Text"}
+        </div>
+        <Textarea value={text} onChange={(e) => setText(e.target.value)}></Textarea>
+        <Button>Export</Button>
       </main>
-      <footer className="">
-
-      </footer>
     </div>
   );
 }
