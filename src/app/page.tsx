36caf4fc1378c444fbfe61e71362137f6989a2c0
huyixi <hi@huyixi.com>
2024-11-27 13:44:23 +0800

    feat: basic export function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 "use client"
 import { useRef, useState } from 'react'
 
+import * as htmlToImage from 'html-to-image'
+import { toPng, toJpeg } from 'html-to-image'
+
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   const [text, setText] = useState('')
   const cardRef = useRef(null)
+
+  const exportAsImage = async () => {
+    if (cardRef.current === null) {
+      return
+    }
+    try {
+      const imageUrl = await toPng(cardRef.current)
+      const link = document.createElement('a')
+      link.href = imageUrl
+      link.download = 'text-card.png'
+      link.click()
+    } catch (error) {
+      console.error("export error", error)
+    }
+  }
 
   return (
     <div className="">
@@ -16,7 +34,7 @@ export default function Home() {
           {text || "Preview Text"}
         </div>
         <Textarea value={text} onChange={(e) => setText(e.target.value)}></Textarea>
-        <Button>Export</Button>
+        <Button onClick={exportAsImage}>Export</Button>
       </main>
     </div>
   );
